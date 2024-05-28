33bfefed135deef9684667639aaa82040b121632
luna.temmerman <luna.temmerman@student.odisee.be>
2024-05-28 11:48:11 +0200

    added express

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-import {makeId, getRoomIndex, findRoom} from "./helpers.js";
 import express from "express";
 import {createServer} from "http";
 import {Server} from "socket.io";
+import {makeId, getRoomIndex, findRoom} from "./helpers.js";
 
 const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
-    origin: ["*"],
-    methods: ["GET", "POST"],
+    origin: "*",
+  },
+  connectionStateRecovery: {
+    maxDisconnectionDuration: 10 * 60 * 1000,
+    skipMiddlewares: true,
   },
 });
 
@@ -51,8 +54,6 @@ const onConnection = (socket) => {
 
 io.on("connection", onConnection);
 
-app.get("/", (req, res) => {
-  res.send("Hello visitor!");
+httpServer.listen(4000, function () {
+  return "<h1>Welcome</h1>";
 });
-
-httpServer.listen(4000);
